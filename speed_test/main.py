e4f9f3445d6e2c6cc1c0d8f14651a7d43a82f800
qiuxiuya <qiuxiuya.sh@gmail.com>
2026-08-03 23:23:25 +0800

    speed_test:同步为一佬的speed test cli

--- speed_test/main.py
+++ speed_test/main.py
@@ -1,65 +1,116 @@
 import contextlib
+import platform
+import tarfile
+
+from asyncio import create_subprocess_shell
+from asyncio.subprocess import PIPE
+from json import loads
 
 from PIL import Image
+from os import makedirs
 from os.path import exists
 from httpx import ReadTimeout
 
 from pagermaid.listener import listener
+from pagermaid.utils import safe_remove
+from pagermaid.utils.bot_utils import edit_delete
 from pagermaid.enums import Client, Message, AsyncClient
-from pagermaid.utils import lang, pip_install, safe_remove
+from pagermaid.utils import lang
 
-pip_install("speedtest-cli", alias="speedtest")
+speedtest_path = "/var/lib/pagermaid/plugins/speedtest-cli/speedtest"
 
-from speedtest import (
-    Speedtest,
-    ShareResultsConnectFailure,
-    ShareResultsSubmitFailure,
-    NoMatchedServers,
-    SpeedtestBestServerFailure,
-    SpeedtestHTTPError,
-)
+async def download_cli(request):
+    speedtest_version = "1.2.0"
+    machine = str(platform.machine())
+    if machine == "AMD64":
+        machine = "x86_64"
+    filename = (f"ookla-speedtest-{speedtest_version}-linux-{machine}.tgz")
+    speedtest_url = (f"https://install.speedtest.net/app/cli/{filename}")
+    path = "/var/lib/pagermaid/plugins/speedtest-cli/"
+    if not exists(path):
+        makedirs(path)
+    data = await request.get(speedtest_url)
+    with open(path+filename, mode="wb") as f:
+        f.write(data.content)
+    try:
+        tar = tarfile.open(path+filename, "r:gz")
+        file_names = tar.getnames()
+        for file_name in file_names:
+            tar.extract(file_name, path)
+        tar.close()
+        safe_remove(path+filename)
+        safe_remove(f"{path}speedtest.5")
+        safe_remove(f"{path}speedtest.md")
+    except Exception:
+        return "解压测速文件失败",None
+    proc = await create_subprocess_shell(
+        f"chmod +x {speedtest_path}",
+        shell=True,
+        stdout=PIPE,
+        stderr=PIPE,
+        stdin=PIPE,
+    )
+    stdout, stderr = await proc.communicate()
+    return path if exists(f"{path}speedtest") else None
 
-
-def unit_convert(byte):
-    """Converts byte into readable formats."""
+async def unit_convert(byte):
+    """ Converts byte into readable formats. """
     power = 1000
     zero = 0
-    units = {0: "", 1: "Kb/s", 2: "Mb/s", 3: "Gb/s", 4: "Tb/s"}
+    units = {
+        0: '',
+        1: 'Kb/s',
+        2: 'Mb/s',
+        3: 'Gb/s',
+        4: 'Tb/s'
+        }
+    byte = byte * 8
     while byte > power:
         byte /= power
         zero += 1
-    return f"{round(byte, 2)} {units[zero]}"
+    return f"{round(byte, 2)} {units[zero]}"  
 
+async def start_speedtest(command):
+    """ Executes command and returns output, with the option of enabling stderr. """
+    proc = await create_subprocess_shell(command,shell=True,stdout=PIPE,stderr=PIPE,stdin=PIPE)
+    stdout, stderr = await proc.communicate()
+    try:
+        stdout = str(stdout.decode().strip())
+        stderr = str(stderr.decode().strip())
+    except UnicodeDecodeError:
+        stdout = str(stdout.decode('gbk').strip())
+        stderr = str(stderr.decode('gbk').strip())
+    return stdout,stderr,proc.returncode
 
 async def run_speedtest(request: AsyncClient, message: Message):
-    test = Speedtest()
-    server = int(message.arguments) if len(message.parameter) == 1 else None
-    if server:
-        servers = test.get_closest_servers()
-        for i in servers:
-            if i["id"] == str(server):
-                test.servers = [i]
-                break
-    test.get_best_server(servers=test.servers)
-    test.download()
-    test.upload()
-    with contextlib.suppress(ShareResultsConnectFailure):
-        test.results.share()
-    result = test.results.dict()
+    if not exists(speedtest_path):
+        await download_cli(request)
+
+    command = (f"sudo {speedtest_path} --accept-license --accept-gdpr -s {message.arguments} -f json") if str.isdigit(message.arguments) else (f"sudo {speedtest_path} --accept-license --accept-gdpr -f json")
+
+    outs,errs,code = await start_speedtest(command)
+    if code == 0:
+        result = loads(outs)
+    elif loads(errs)['message'] == "Configuration - No servers defined (NoServersException)":
+        return "无法连接到指定服务器",None
+    else:
+        return lang('speedtest_ConnectFailure'),None
+
+
     des = (
         f"**Speedtest** \n"
         f"Server: `{result['server']['name']} - "
-        f"{result['server']['cc']}` \n"
-        f"Sponsor: `{result['server']['sponsor']}` \n"
-        f"Upload: `{unit_convert(result['upload'])}` \n"
-        f"Download: `{unit_convert(result['download'])}` \n"
-        f"Latency: `{result['ping']}` \n"
+        f"{result['server']['id']}` \n"
+        f"Location: `{result['server']['location']}` \n"
+        f"Upload: `{await unit_convert(result['upload']['bandwidth'])}` \n"
+        f"Download: `{await unit_convert(result['download']['bandwidth'])}` \n"
+        f"Latency: `{result['ping']['latency']} ms`\n"
         f"Timestamp: `{result['timestamp']}`"
+        #f"\nDebug: `\nmessage.arguments.len:{len(message.arguments)}\nresult_str: {outs}\nerrs:{errs}\nreturncode:{code}`"
     )
-    if result["share"]:
-        data = await request.get(
-            result["share"].replace("http:", "https:"), follow_redirects=True
-        )
+
+    if result["result"]["url"]:
+        data =  await request.get(result["result"]["url"]+'.png')
         with open("speedtest.png", mode="wb") as f:
             f.write(data.content)
         with contextlib.suppress(Exception):
@@ -69,56 +120,54 @@ async def run_speedtest(request: AsyncClient, message: Message):
     return des, "speedtest.png" if exists("speedtest.png") else None
 
 
-async def get_all_ids():
-    test = Speedtest()
-    servers = test.get_closest_servers()
+async def get_all_ids(request):
+    """ Get speedtest_server. """
+    if not exists(speedtest_path):
+        await download_cli(request)
+    outs,errs,code = await start_speedtest(f"sudo {speedtest_path} -f json -L")
+    result = loads(outs) if code == 0 else None
     return (
         (
-            "附近的测速点有：\n\n"
+            "附近的测速点有：\n"
             + "\n".join(
-                f"`{i['id']}` - `{int(i['d'])}km` - `{i['name']}` - `{i['sponsor']}`"
-                for i in servers
+                f"`{i['id']}` - `{i['name']}` - `{i['location']}`"
+                for i in result['servers']
             ),
             None,
         )
-        if servers
+        if result
         else ("附近没有测速点", None)
     )
 
-
-@listener(
-    command="speedtest",
-    description=lang("speedtest_des"),
-    parameters="(Server ID/测速点列表)",
-)
+@listener(command="sp",
+          need_admin=True,
+          description=lang('speedtest_des'),
+          parameters="(list/server id)")
 async def speedtest(client: Client, message: Message, request: AsyncClient):
-    """Tests internet speed using speedtest."""
-    if message.arguments == "测速点列表":
-        msg = message
+    """ Tests internet speed using speedtest. """
+    msg = message
+    if message.arguments == "list":
+        des, photo = await get_all_ids(request)
+    elif len(message.arguments) == 0 or str.isdigit(message.arguments):
+        msg: Message = await message.edit(lang('speedtest_processing'))
+        des, photo = await run_speedtest(request,message)
     else:
-        msg: Message = await message.edit(lang("speedtest_processing"))
-    try:
-        if message.arguments == "测速点列表":
-            des, photo = await get_all_ids()
-        else:
-            des, photo = await run_speedtest(request, message)
-    except SpeedtestHTTPError:
-        return await msg.edit(lang("speedtest_ConnectFailure"))
-    except (ValueError, TypeError):
-        return await msg.edit(lang("arg_error"))
-    except (SpeedtestBestServerFailure, NoMatchedServers):
-        return await msg.edit(lang("speedtest_ServerFailure"))
-    except (ShareResultsSubmitFailure, RuntimeError, ReadTimeout):
-        return await msg.edit(lang("speedtest_ConnectFailure"))
+        return await msg.edit(lang('arg_error'))
     if not photo:
         return await msg.edit(des)
     try:
-        await client.send_photo(
-            message.chat.id,
-            photo,
-            caption=des,
-            message_thread_id=message.message_thread_id or message.reply_to_message_id,
-        )
+        # await client.send_photo(message.chat.id, photo, caption=des)
+        # await message.reply_photo(
+        #     photo,
+        #     caption=des,
+        #     quote=False,
+        #     reply_to_message_id=message.reply_to_top_message_id,
+        # )
+        if message.reply_to_message:
+            await message.reply_to_message.reply_photo(photo, caption=des)
+        else:
+            await message.reply_photo(photo, caption=des, quote=False,reply_to_message_id=message.reply_to_top_message_id)
+        await message.safe_delete()
     except Exception:
         return await msg.edit(des)
     await msg.safe_delete()
